0afda0cec60d2a781dfc484c47a5329c0f352dac
Matt D <dwinell.matt@gmail.com>
2019-06-17 13:19:40 -0400

    added js file for burger

---
+++ config/connection.js
@@ -0,0 +1,8 @@
+var sql = require ('mysql');
+var connection = sql.createConnection({
+    host: "localhost",
+    port: 3306,
+    user: "root",
+    password: "25064c001023818v",
+    database: "bamazon_db"
+});
---
+++ db/seeds.sql
@@ -0,0 +1,2 @@
+INSERT INTO burgers (burger_name, devoured)
+VALUES  ("bacon on a bun", false), ("gouda and lamb", true), ("twenty pound burger", false);
